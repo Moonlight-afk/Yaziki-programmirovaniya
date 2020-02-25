49b1de6d413340616700a1cd6bd0956a4ffb5ae2
denis2810 <56162549+denis2810@users.noreply.github.com>
2020-02-25 14:18:06 +0700

    Update Laba-1 zadanie-1.cpp

--- Laba 1/Laba-1 zadanie-1.cpp
+++ Laba 1/Laba-1 zadanie-1.cpp
@@ -7,28 +7,37 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int n,k=0;
+	int n, k = 0;
 	cout << "Введите размер массива:";
 	cin >> n;
-	int *a = new int[n];
-	ofstream newFile;
-	newFile.open("D:\\Work\\Ilya\\Project22\\massiv.txt");
-	if (newFile.is_open()) {
-		while (k < n) {
-			a[k] = k;
-			newFile << k << endl;
-			k++;
+	if (n < 5) {
+		while(n<5){
+			cout << "Некорректный ввод" << endl;
+			cout << "Введите размер массива:";
+			cin >> n;
 		}
 	}
 	else {
-		cout << "Файл не может быть открыт.";
+		int *a = new int[n];
+		ofstream newFile;
+		newFile.open("D:\\Work\\Ilya\\Project22\\massiv.txt");
+		if (newFile.is_open()) {
+			while (k < n) {
+				a[k] = k;
+				newFile << k << endl;
+				k++;
+			}
+		}
+		else {
+			cout << "Файл не может быть открыт.";
+		}
+		string line;
+		ifstream in("D:\\Work\\Ilya\\Project22\\massiv.txt");
+		while (!in.eof())
+		{
+			getline(in, line);
+			cout << line << endl;
+		}
+		system("pause");
 	}
-	string line;
-	ifstream in("D:\\Work\\Ilya\\Project22\\massiv.txt");
-	while (!in.eof())
-	{
-		getline(in, line);
-		cout << line << endl;
-	}
-	system("pause");
 }
