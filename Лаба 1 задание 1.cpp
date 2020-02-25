b3d38fc7e3c46a22fa03fef8bc1e44857337fc22
denis2810 <56162549+denis2810@users.noreply.github.com>
2020-02-25 15:03:56 +0700

    Update Лаба 1 задание 1.cpp

--- Лаба 1 задание 1.cpp
+++ Лаба 1 задание 1.cpp
@@ -2,37 +2,69 @@
 #include<math.h>
 #include<fstream>
 #include<string>
+#include<conio.h>
 
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int n,k=0;
+	int chet=0, n,i=0, k = 0;
 	cout << "Введите размер массива:";
 	cin >> n;
-	int *a = new int[n];
-	fstream file;
-	file.open("D:\\Work\\Ilya\\Project22\\massiv.txt");
-	if (file.is_open()) {
-		while (k < n) {
-			a[k] = k;
-			file << k << " ";
-			k++;
+	if (n < 5) {
+		while (n<5) {
+			cout << "Некорректный ввод" << endl;
+			cout << "Введите размер массива:";
+			cin >> n;
 		}
 	}
-	else {
-		cout << "Файл не может быть открыт.";
-	}
-	string line;
-	ifstream in("D:\\Work\\Ilya\\Project22\\massiv.txt");
-	if (in.is_open()) {
-		while (getline(in, line)) {
-			cout << line;
+	
+		int *a = new int[n];
+		/*FILE* fin = NULL;
+		fin = fopen("D:\\Work\\Ilya\\Project22\\massiv.txt","r");
+		//ifstream newFile("D:\\Work\\Ilya\\Project22\\massiv.txt");
+		if (!fin) {
+			cout << "Ошибка" << endl;
 		}
-	}
-	else {
-		cout << "Файл не может быть открыт.";
-	}
-	in.close();
-	system("pause");
+		else {
+			while (i < n) {
+				fscanf(fin, "%d", &a[i]);
+				i++;
+			}
+		}
+		fclose(fin);*/
+		ifstream out("D:\\Work\\Ilya\\Project22\\massiv.txt", ios::out);
+		if (!out) {
+			cout << "Ошибка" << endl;
+		}
+		else {
+			for (int i = 0; i < n; i++) {
+				out.read((char*)&a[i], sizeof(i));
+				out.close();
+				char ch = getchar();
+			}
+		}
+		i = 0;
+		while (i < n) {
+			if (a[i] >= 0) {
+				chet++;
+			}
+			i++;
+		}
+		i = 0;
+		int m = 0;
+		int *nov = new int[chet];
+		while (i < n) {
+			if (a[i] >= 0) {
+				nov[m] = a[i];
+				m++;
+			}
+			i++;
+		}
+		m = 0;
+		while (m < chet) {
+			cout << nov[m];
+			i++;
+		}
+	
 }
