6b1ddfb14cab38e3edef6a1a91e4caba6a23e4af
denis2810 <56162549+denis2810@users.noreply.github.com>
2020-03-03 14:24:01 +0700

    Update Laba-1 zadanie-1.cpp

--- Laba 1/Laba-1 zadanie-1.cpp
+++ Laba 1/Laba-1 zadanie-1.cpp
@@ -1,3 +1,4 @@
+#include"stdafx.h"
 #include<iostream>
 #include<math.h>
 #include<fstream>
@@ -8,32 +9,30 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int chet = 0, n, i = 0, k = 0;
-	cout << "Введите размер массива:";
-	cin >> n;
-	if (n < 5) {
-		while (n < 5) {
-			cout << "Некорректный ввод" << endl;
-			cout << "Введите размер массива:";
-			cin >> n;
-		}
-	}
-	int* a = new int[n];
+	int chet = 0, i = 0, k = 0;
+	int* a = new int[100];
 	//открываем файл
-	ifstream file("C:\\Users\\стас\\source\\repos\\Project2\\massiv.txt");
+	ifstream file("C:\\Users\\student\\Desktop\\massiv.txt");
 	if (!file) {
 		cout << "Ошибка" << endl;
 	}
 	else {
 		//заполняем массив числами из файл
-		while (i < n) {
+		while (i < 100) {
 			file >> a[i];
 			i++;
 		}
 		cout << endl;
-		for (int i = 0; i < n; i++) {
+		i = 0;
+		cout << "Неотсортированный:" << endl;
+		while (i < 100) {
+			cout << a[i] << " ";
+			i++;
+		}
+		cout << endl;
+		for (int i = 0; i < 100; i++) {
 			bool flag = true;
-			for (int j = 0; j < n - (i + 1); j++) {
+			for (int j = 0; j < 100 - (i + 1); j++) {
 				if (a[j] > a[j + 1]) {
 					flag = false;
 					swap(a[j], a[j + 1]);
@@ -44,11 +43,29 @@ int main() {
 			}
 		}
 		i = 0;
-		while (i < n) {
-			cout << a[i] << " ";
+		while (i < 100) {
+			if (a[i] >= 0) {
+				k++;
+			}
+			i++;
+		}
+		int* nov = new int[k];
+		int j = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			if (a[i] >= 0)
+			{
+				nov[j] = a[i];
+				j++;
+			}
+		}
+		cout << "Отсортированный:" << endl;
+		i = 0;
+		while (i < k) {
+			cout << nov[i] << " ";
 			i++;
 		}
 		cout << endl;
-		system("pause");
 	}
+	system("pause");
 }
