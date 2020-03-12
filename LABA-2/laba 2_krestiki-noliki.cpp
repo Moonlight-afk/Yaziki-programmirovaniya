7178fdcdf087f6032f7380b86544bf45294659aa
denis2810 <56162549+denis2810@users.noreply.github.com>
2020-03-12 17:48:37 +0700

    Update laba 2_krestiki-noliki.cpp

--- LABA-2/laba 2_krestiki-noliki.cpp
+++ LABA-2/laba 2_krestiki-noliki.cpp
@@ -1,41 +1,51 @@
 #include<iostream>
-
+#include <cstdlib>
 
 char board[9] = { '-','-' ,'-' ,'-' ,'-' ,'-' ,'-' ,'-' ,'-' };
 int i, j;
+
+//функция вывода возможных ходов
 void board_cout() {
-	cout << "#############" << endl;
-	cout << "# 1 # 2 # 3 #" << endl;
-	cout << "#############" << endl;
-	cout << "# 4 # 5 # 6 #" << endl;
-	cout << "#############" << endl;
-	cout << "# 7 # 8 # 9 #" << endl;
-	cout << "#############" << endl;
+	std::cout << "#############" << std::endl;
+	std::cout << "# 1 # 2 # 3 #" << std::endl;
+	std::cout << "#############" << std::endl;
+	std::cout << "# 4 # 5 # 6 #" << std::endl;
+	std::cout << "#############" << std::endl;
+	std::cout << "# 7 # 8 # 9 #" << std::endl;
+	std::cout << "#############" << std::endl;
 }
 
+
+//функция вывода доски с ходами
 int move() {
 	int nomer;
-	cout << "#############" << endl;
-	cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << endl;
-	cout << "#############" << endl;
-	cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << endl;
-	cout << "#############" << endl;
-	cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << endl;
-	cout << "#############" << endl;
-	cout << "В какую ячейку вы хотите поставить Х?-";
-	cin >> nomer;
+	std::cout << "#############" << std::endl;
+	std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+	std::cout << "#############" << std::endl;
+	std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+	std::cout << "#############" << std::endl;
+	std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+	std::cout << "#############" << std::endl;
+	std::cout << "В какую ячейку вы хотите поставить Х?-";
+	std::cin >> nomer;
 	while (nomer > 9 || nomer < 1) {
-		cout << "Пожалуйста введите правильное число (1-9):";
-		cin >> nomer;
+		std::cout << "Пожалуйста введите правильное число (1-9):";
+		std::cin >> nomer;
 	}
 	return nomer;
 }
+
+
 using namespace std;
+
+
 int main() {
 	setlocale(LC_ALL, "RUS");
 	int** a;
 	int n = 3, m = 3, proverka;
-	a = new int*[n];
+
+	//задаем массив и заполняем нулями
+	a = new int* [n];
 	for (i = 0; i < n; i++) {
 		a[i] = new int[m];
 	}
@@ -44,27 +54,14 @@ int main() {
 			a[i][j] = 0;
 		}
 	}
-	/*int a11 = a[0][0];
-	int a12 = a[0][1];
-	int a13 = a[0][2];
-	int a21 = a[1][0];
-	int a22 = a[1][1];
-	int a23 = a[1][2];
-	int a31 = a[2][0];
-	int a32 = a[2][1];
-	int a33 = a[2][2];*/
-	/*cout << "Массив:" << endl;
-	for (i = 0; i < n; i++)
-	{
 
-		for (j = 0; j < m; j++)
-			printf("%5d", a[i][j]);
-		cout << endl;
-	}*/
+	//вызываем 2 функции и выбираем ход
 	board_cout();
 	cout << endl;
 	int nomer = move();
-	board[nomer - 1] = 'X';
+	board[nomer - 1] = 'X'; //поставит Х в выбранную область
+
+	//условия для заполнения массива
 	if (nomer == 1) {
 		a[0][0] = 1;
 	}
@@ -92,20 +89,17 @@ int main() {
 	if (nomer == 9) {
 		a[2][2] = 1;
 	}
-	/*cout << "Массив:" << endl;
-	for (i = 0; i < n; i++)
-	{
 
-		for (j = 0; j < m; j++)
-			printf("%5d", a[i][j]);
-		cout << endl;
-	}*/
+	//сканируем массив первый раз
 	for (i = 0; i < n; i++)
 	{
 
 		for (j = 0; j < m; j++) {
+			//если находим ноль, то вместо него ставим единицу и в игральное поле ставиться О
 			if (a[i][j] == 0) {
 				a[i][j] = 1;
+
+				//условия чтобы ноль встал на нужное место
 				if (i == 0 && j == 0) {
 					board[0] = 'O';
 				}
@@ -133,33 +127,27 @@ int main() {
 				if (i == 2 && j == 2) {
 					board[8] = 'O';
 				}
-				proverka = 1;
+				proverka = 1; //проверна нужна чтобы выходить из цикла
 				break;
 			}
 		}
 		if (proverka = 1)break;
 	}
 
-	///////////////////////////////
-	for (int k = 1; k < 5; k++) {
-		system("cls");
-		if (board[0] == 'X'&&board[1] == 'X'&&board[2] == 'X') {
-			cout << "Выиграли крестики!" << endl;
-		}
-		//cout << "shag:" << k<<endl;
-		proverka = 0;
-		/*cout << "Массив:" << endl;
-		for (i = 0; i < n; i++)
-		{
 
-			for (j = 0; j < m; j++)
-				printf("%5d", a[i][j]);
-			cout << endl;
-		}*/
+	//главный цикл заполнения поля нулями и крестиками
+	for (int k = 1; k < 5; k++) {
+		system("cls");  //очищаем терминал
+		proverka = 0;   //зануляем проверку каждый раз
+
+
+		//повтореня действий описанных выше
 		board_cout();
 		cout << endl;
 		int nomer = move();
 		board[nomer - 1] = 'X';
+
+
 		if (nomer == 1) {
 			a[0][0] = 1;
 		}
@@ -187,22 +175,117 @@ int main() {
 		if (nomer == 9) {
 			a[2][2] = 1;
 		}
-		/*cout << "Массив:" << endl;
-		for (i = 0; i < n; i++)
-		{
 
-			for (j = 0; j < m; j++)
-				printf("%5d", a[i][j]);
-			cout << endl;
-		}*/
+
+		//каждый раз после хода крестиками идет проверка выиграша
+		if (board[0] == 'X' && board[1] == 'X' && board[2] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+		if (board[3] == 'X' && board[4] == 'X' && board[5] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+		if (board[6] == 'X' && board[7] == 'X' && board[8] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+		if (board[0] == 'X' && board[3] == 'X' && board[6] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+		if (board[1] == 'X' && board[4] == 'X' && board[7] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+		if (board[2] == 'X' && board[5] == 'X' && board[8] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+		if (board[0] == 'X' && board[4] == 'X' && board[8] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+		if (board[2] == 'X' && board[4] == 'X' && board[6] == 'X') {
+			cout << "Выиграли крестики!" << endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+			std::cout << "#############" << std::endl;
+			system("pause");
+			exit(0);
+		}
+
+
+		//сканирование массива, чтобы в пустую область компьютер поставил нолик
 		if (proverka == 0) {
-			//cout << "norm" << endl;
 			for (i = 0; i < n; i++)
 			{
 
 				for (j = 0; j < m; j++) {
 					if (a[i][j] == 0) {
 						a[i][j] = 1;
+
+
 						if (i == 0 && j == 0) {
 							board[0] = 'O';
 						}
@@ -230,15 +313,126 @@ int main() {
 						if (i == 2 && j == 2) {
 							board[8] = 'O';
 						}
+
+
+						//проверка на выигрыш после хода ноликами
+						if (board[0] == 'O' && board[1] == 'O' && board[2] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+						if (board[3] == 'O' && board[4] == 'O' && board[5] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+						if (board[6] == 'O' && board[7] == 'O' && board[8] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+						if (board[0] == 'O' && board[3] == 'O' && board[6] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+						if (board[1] == 'O' && board[4] == 'O' && board[7] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+						if (board[2] == 'O' && board[5] == 'O' && board[8] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+						if (board[0] == 'O' && board[4] == 'O' && board[8] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+						if (board[2] == 'O' && board[4] == 'O' && board[6] == 'O') {
+							system("cls");
+							cout << "Выиграли нолики!" << endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[3] << " # " << board[4] << " # " << board[5] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							std::cout << "# " << board[6] << " # " << board[7] << " # " << board[8] << " # " << std::endl;
+							std::cout << "#############" << std::endl;
+							system("pause");
+							exit(0);
+						}
+
+
 						proverka = 1;
+
 					}
-					if(proverka==1)break;
+					if (proverka == 1)break;
 				}
 				if (proverka == 1)break;
 			}
 		}
-		//else cout << "ne norm" << endl;
 	}
+
+	//если никто не выиграл, очищается терминал и выводится получившееся поле
 	system("cls");
 	cout << "#############" << endl;
 	cout << "# " << board[0] << " # " << board[1] << " # " << board[2] << " # " << endl;
