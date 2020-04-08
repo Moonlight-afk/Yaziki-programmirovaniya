fa03f5900a0b3c05f6495283805edf791b4f0853
denis2810 <56162549+denis2810@users.noreply.github.com>
2020-04-08 17:34:54 +0700

    Update zadanie2.cpp

--- LABA-2/zadanie2.cpp
+++ LABA-2/zadanie2.cpp
@@ -5,31 +5,21 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "RUS");
-	int n, m, ** A, ** C,i,j;
+	int n, m, ** A, ** C, i, j;
 	ifstream file;
-	cout << "Введите число строк и столбцов которые будут содержать матрицы:"<<endl;
+	cout << "Введите число строк и столбцов которые будут содержать матрицы:" << endl;
 	cout << "Строк:";
 	cin >> m;
 	cout << "Столбцов:";
 	cin >> n;
-	/*if (n != m) {
-		while (m != n) {
-			cout << "Не получится вычислить выражение из за разного размера матриц, пожалуйста введите еще раз." << endl;
-			cout << "Введите число строк и столбцов которые будут содержать матрицы:" << endl;
-			cout << "Строк:";
-			cin >> m;
-			cout << "Столбцов:";
-			cin >> n;
-		}
-	}*/
 	A = new int* [n];
 	C = new int* [m];
-	int *B = new int[n];
+	int* B = new int[n];
 	int* D = new int[m];
-    file.open("C:\\Users\\стас\\Desktop\\massiv.txt");
+	file.open("C:\\Users\\стас\\Desktop\\massiv.txt");
 	if (!file.is_open()) {
-			cout << "Файл не может быть открыт";
-			exit(0);
+		cout << "Файл не может быть открыт";
+		exit(0);
 	}
 	for (int i = 0; i < n; i++)
 	{
@@ -55,7 +45,7 @@ int main() {
 		}
 		cout << endl;
 	}
-	
+
 	cout << "Матрица С:" << endl;
 	for (int i = 0; i < m; i++)
 	{
@@ -73,7 +63,7 @@ int main() {
 	cout << "Вектор В:" << endl;
 	for (i = 0; i < n; i++) {
 		file >> B[i];
-		cout << B[i] << " ";
+		cout << B[i] << endl;
 	}
 	cout << endl;
 	cout << "Вектор D:" << endl;
@@ -91,79 +81,37 @@ int main() {
 	int* E = new int[n];
 	for (i = 0; i < n; i++) {
 		E[i] = 0;
-		//cout << E[i] << " ";
 	}
 	cout << endl;
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < m; j++) {
-			E[i] =E[i]+( C[j][i] * D[j]);
-			//cout << C[j][i] << "---C " << endl;;
-			//cout << D[j] << "---D" << endl;
-			//cout << E[k] << " ";
+			E[i] = E[i] + (C[j][i] * D[j]);
 		}
 		cout << E[i] << " ";
 	}
 	cout << endl;
 	cout << "Умножение вектора В на полученную матрицу Е:" << endl;
-	int* F = new int{ 0 };
-	for (i = 0; i < m; i++) {
-		F[0] = F[0] + (E[i] * B[i]);
-	}
-	cout << F[0] << endl;
-	/*for (i = 0; i < n; i++) {
-		cout << E[i] << " ";
-	}*/
-	/*cout << "Умножение вектора В на вектор D:" << endl;
-	int k = -1;
-	int l = 0;
-	int** BD;
-	BD = new int*[n];
+	int** F = new int* [n];
 	for (int i = 0; i < n; i++)
 	{
-		BD[i] = new int[m];
+		F[i] = new int[n];
 	}
 	for (int i = 0; i < n; i++) {
-		k ++;
-		l = 0;
-		for (int j = 0; j < m; j++) {
-			BD[i][j] = B[k] * D[l];
-			l++;
-			cout << BD[i][j] << "\t";
-		}
-		cout << endl;
-	}*/
-	/*cout << "Умножение вектора B на матрицу С:" << endl;
-	int k = 0;
-	for (int i = 0; i < m; i++) {
-		k = 0;
 		for (int j = 0; j < n; j++) {
-			C[i][j] += C[i][j] * B[k];
-			k++;
-			cout << C[i][j] << "\t";
+			F[i][j] = B[i] * E[j];
+			cout << F[i][j] << "\t";
 		}
 		cout << endl;
 	}
-	cout << endl;
-	cout << "Умножение вектора D на матрицу C:" << endl;
-	k = 0;
-	for (int i = 0; i < m; i++) {
-		k = 0;
-		for (int j = 0; j < n; j++) {
-			C[i][j] = C[i][j] * D[k];
-			k++;
-			cout << C[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
+	
 	cout << "Ответ:" << endl;
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			A[i][j] = A[i][j] + C[i][j];
-			cout << C[i][j] << "\t";
+			A[i][j] = A[i][j] + F[i][j];
+			cout << A[i][j] << "\t";
 		}
 		cout << endl;
-	}*/
+	}
 	cout << endl;
 	system("pause");
 }
